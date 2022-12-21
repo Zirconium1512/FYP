68f8bf6f7a94f3f23de6f7a823d974c4fa43a7d1
Aniket Ray <aniketrayspeedz@gmail.com>
2022-12-21 09:07:37 +0530

    initial commit

---
+++ temp.py
@@ -0,0 +1,7 @@
+import cv2
+import torch 
+print(torch.cuda.is_available())
+
+# img = cv2.imread('C:\\Users\\Zirconium\\OneDrive\\Desktop\\simulator\\Images\\IMG\\right_2022_12_20_21_49_51_162.jpg')
+
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
